7230843f3b7bc414e96fc7dea7a3e1ee57893d26
fpeña <fpena@asesoftware.com>
2021-06-22 12:26:38 -0500

    First Commit

---
+++ src/app/country/pages/view-country/view-country.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-country',
+  templateUrl: './view-country.component.html',
+})
+export class ViewCountryComponent {
+ 
+}
